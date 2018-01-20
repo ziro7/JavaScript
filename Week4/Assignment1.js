0a38f0e4363683d69aa5271854c4b71a77157e6e
Jacob Kjaergaard <upperiq@hotmail.com>
2018-01-20 21:01:28 +0100

    assignments week4

---
+++ Week4/Assignment1.js
@@ -0,0 +1,3 @@
+//Lav et Hang-man spil. Computeren skal tænke på et ord, som brugeren skal prøve at gætte. 
+
+//Hver gang brugeren indtaster et bogstav, skal det vises, hvor i ordet, bogstavet findes.
