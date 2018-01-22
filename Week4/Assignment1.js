5e494da45e26a191ca8f655f4af9c76c7b82fcfd
Jacob Kjaergaard <upperiq@hotmail.com>
2018-01-22 22:37:10 +0100

    week4

--- Week4/Assignment1.js
+++ Week4/Assignment1.js
@@ -4,7 +4,7 @@
 
 /*
 Lav en variable med ordet som string.
-Lav en slice splice og og itererer over dem hvor de pushes til en tom array, så array er som string men med et char pr index
+Lav en split og og itererer over dem hvor de pushes til en tom array, så array er som string men med et char pr index
 Lav en tom array
 Iterere over den fyldte array og for hvert index i length sæt et underskår i den tomme array.
 
@@ -18,3 +18,56 @@ Hvis tomme array er lig med fyldte array har man vundet.
 
 
 */
+var word = prompt("Enter word to guess");
+var wordArray = word.split('');
+console.log (wordArray);
+var empty = [];
+
+for (i=0; i<wordArray.length; i++){
+    if (wordArray[i]!= null){
+        empty[i] = "_";
+    }
+}
+console.log(empty);
+
+var tries = 0;
+var correct = 0;
+
+document.getElementById("panel").innerHTML = "The word is: " + empty;
+
+function hangman(){
+
+    var guess = document.getElementById("letter").value;
+
+    for (i=0; i<empty.length;i++){
+        if (guess === wordArray[i]){
+            empty[i] = guess;
+            correct+= 1;
+            console.log("correct is: " + correct);
+        } 
+    }
+
+    if (correct == 0){
+        tries += 1;
+        correct = 0;
+    } else {
+        correct = 0;
+    }
+     
+    if (JSON.stringify(wordArray)==JSON.stringify(empty)){
+        alert("You Won!")
+    }
+
+    if (tries > 6){
+        console.log("You have failed");
+    }
+    
+    document.getElementById("panel").innerHTML = "The Number of failed attempts: " + tries + " The word is: " + empty;
+    console.log("tries is: " + tries);
+    
+}
+
+
+
+
+
