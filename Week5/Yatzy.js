25877535ec9deac34cdd99828f0fad7d361ae980
Jacob Kjaergaard <upperiq@hotmail.com>
2018-01-30 21:53:59 +0100

    week5

--- Week5/Yatzy.js
+++ Week5/Yatzy.js
@@ -1,101 +1,112 @@
 
-
-
+//funktion til den øverste del med 1'ere eller 2'ere.
 function topSection(terninger,counter ) {
     var point = 0;
     for (i = 0; i < terninger.length; i++) {
-      if (terninger[i] == counter) { 
-        point += counter; 
-      }
+        if (terninger[i] == counter) { 
+            point += counter; 
+        }
     }
     return point;
 }
 
-console.log(topSection([1,2,3,4,5],1));
-console.log(topSection([1,2,3,4,5],2));
-console.log(topSection([1,2,3,4,5],3));
-console.log(topSection([1,2,3,4,5],4));
-console.log(topSection([1,2,3,4,5],5));
-console.log(topSection([6,6,6,6,6],6));
-
-/*
-To par
-Tre ens
-Fire ens
-Lav
-Høj
-Fuldt hus
-Chance
-Yatzy
-*/
-
+//hjælpe funktion som filtre array til at består af den der kun har "værdi"
 function arrayMedVærdi(terninger, værdi){
     var arrayMedVærdi = [];
     for (i=0; i<terninger.length; i++){
         if (terninger[i]==værdi){
             arrayMedVærdi.push(værdi);
         }
+    }
     return arrayMedVærdi;
 }
 
+//hjælpe funktion tælle en array sammen.
+function sum(terninger){
+             
+    var sum =  0;
+    for(var i=0;i<terninger.length;i++){                  
+        sum += Number(terninger[i]);
+    }
+    return sum;
+}
 
 //et par
 function etPar (terninger, parIHvad) {
     
-    var filteredArray = [];
+    var filteredArray = arrayMedVærdi(terninger,parIHvad);
     var result = 0;
 
-    for (i=0; i<terninger.length; i++){
-        if (terninger[i]==parIHvad){
-            filteredArray.push(parIHvad);
-        }
-    }
-    console.log(filteredArray);
-
     if (filteredArray.length>1){
-        var result = filteredArray[0] + filteredArray[1];
+        var result = filteredArray[0] + filteredArray[1]; //hvis der er 3 ens vil den kun tage værdi af de først 2.
     }
 
     return result;
 }
-
-//test af et par.
-console.log("et par med [1,2,3,4,5] og 3: " + etPar([1,2,3,4,5],3))
-console.log("et par med [1,2,3,3,4] og 3: " + etPar([1,2,3,3,4],3))
-console.log("et par med [1,2,3,3,3] og 3: " + etPar([1,2,3,3,3],3))
-//hvis der er 3 ens vil den kun tage værdi af de først 2.
 
 //to par
 function toPar (terninger, parEt, parTo) {
     
-    var filteredArray = arrayMedVærdi(terninger,parEt);
+    var ArrayOne = arrayMedVærdi(terninger,parEt);
+    var ArrayTwo = arrayMedVærdi(terninger,parTo);
     var result = 0;
     
-    console.log(filteredArray);
-
-    if (filteredArray.length>1){
-        var result = filteredArray[0] + filteredArray[1];
+    if (ArrayOne.length>1 && ArrayTwo.length>1 ){
+        var result = ArrayOne[0] + ArrayOne[1] + ArrayTwo[0] + ArrayTwo[1];
     }
 
     return result;
 }
 
-console.log("et par med [1,2,3,4,5] og 3: " + toPar([1,2,3,4,5],3));
-console.log("et par med [1,2,3,3,4] og 3: " + toPar([1,2,3,3,4],3));
-console.log("et par med [1,2,3,3,3] og 3: " + toPar([1,2,3,3,3],3));
+//tre, fire og fem ens
+function ens (terninger, ensIHvad) {
+    
+    var filteredArray = arrayMedVærdi(terninger,ensIHvad);
+    var result = 0;
 
-//hvis der er fuldt hus, vil den kun tage værdi af de to første par..
+    if (filteredArray.length>2){
+        var result = sum(filteredArray);
+    }
 
+    return result;
+}
 
-/*
-YATSY
+//Chancen
+function Chancen(terninger){
+    var result = sum(terninger);
+    return result;
+}
 
-every() metoden er nok bedre til Yatsy - da den false eller true hvis alle værdi matcher.
+//Lav
+function lav(terninger){
+    var result = 0;
+    if (JSON.stringify(terninger)==JSON.stringify([1,2,3,4,5])){
+        result = 15;
+    } 
+    return result;
+}
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+//Høj
+function høj(terninger){
+    var result = 0;
+    if (JSON.stringify(terninger)==JSON.stringify([2,3,4,5,6])){
+        result = 20;
+    } 
+    return result;
+}
 
+//fuldt hus
+function fuldtHus (terninger, par, ensIHvad) {
+    
+    var ArrayOne = arrayMedVærdi(terninger,par);
+    var ArrayTwo = arrayMedVærdi(terninger,ensIHvad);
+    var result = 0;
 
+    console.log(ArrayOne + " og nummer " + ArrayTwo);
+    
+    if (ArrayOne.length>1 && ArrayTwo.length>2 ){
+        var result = ArrayOne[0] + ArrayOne[1] + ArrayTwo[0] + ArrayTwo[1] + ArrayTwo[2];
+    }
 
-Kan måske give terninger med og se om alle matcher terninger[0];
-*/
+    return result;
 }
